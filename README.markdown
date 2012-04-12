94a1a5bbb7a9a5fa1401b93054b8e4cac8d41acc
Dave Woodall <dmwoodall@gmail.com>
2012-04-12 15:49:35 -0500

    adding files

---
+++ README.markdown
@@ -0,0 +1,4 @@
+#Hello World!#
+##[I am](http://dave.woodalls.me) attending [Code Academy's](http://www.codeacademy.org) Spring 2012 class##
+Compiling my notes in this repo.
+
---
+++ amazon.rb
@@ -0,0 +1,40 @@
+# Long challenge: Based on the following data,
+#   write code that prints out the customer's total, including estimated
+#   sales tax, based on the shipping address they entered.
+# Your code should work even if the values of the data change.
+
+# Your output should look like this: "Your total with tax is $4455.54."
+
+
+shopping_cart = [
+  {:name => "iPad 2", :price => 499, :quantity => 2},
+  {:name => "iMac 27", :price => 1699, :quantity => 1},
+  {:name => "MacBook Air 13", :price => 1299, :quantity => 1}
+  ]
+  
+sales_tax = {"IL" => 0.115, "IN" => 0.09, "MI" => 0.06, "WI" => 0.056}
+
+params = {
+  :name => "Patrick McProgrammerson",
+  :address1 => "222 W. Merchandise Mart Plaza",
+  :address2 => "12th Floor",
+  :city => "Chicago",
+  :state => "IL",
+  :zip => "60654"
+  }
+
+def format_total
+	puts "Your total with tax is $#{}"
+end
+
+def total
+	format_total
+end  
+
+# Now change the value of the key :state in the params hash to "WI" and run your code again.
+
+# Encapsulate your code in a method if you haven't already.
+# The method should accept a cart, a hash containing tax rates, and an
+#   address as arguments.
+
+# Now change the quantity of iMacs in the data to 3 and run your method.
