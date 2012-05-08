77fcb05eda65291aed5f52e0b47665de256e567b
Dave Woodall <dmwoodall@gmail.com>
2012-05-07 20:35:49 -0500

    cram session

---
+++ 4th_week/articlesapp/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Articlesapp::Application.config.secret_token = '667453b095d03d884f7e2128b75280b6d34a78007f8b2cca1293b6f51dc94e3095e0cbdfdc7e8f305077cb94f498529d65ece6d3837c63a77f7caf64b14a7d89'
---
+++ 4th_week/blog/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '546b87a64733cda5167bb7519c4a51d1a5dcfce0075d3993d2b64f91d645cd1432f5cbb69e7e5d5592a7d59b8048fcae322f76de60e3794a9708bf5fa82b1586'
